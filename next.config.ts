415cf1a2c93435b3184e00d7ffa4375a7c3ef76a
pjoyet <pierre.joyet@pm.me>
2026-06-30 21:01:04 +0200

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    basePath: isGithubPages ? "/lanelet-map-editor" : "",
+    assetPrefix: isGithubPages ? "/lanelet-map-editor/" : "",
 };
 
 export default nextConfig;
